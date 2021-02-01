079a6eab2c05d7d4a23fc3fb370bfae22fd47a4f
DarkhShines <dardotalain@gmail.com>
2021-02-01 14:30:46 +0100

    Commit Formateur 1.1

--- src/metier/Formateur/Formateur.java
+++ src/metier/Formateur/Formateur.java
@@ -1,6 +1,40 @@
 package metier.Formateur;
 
-public class Formateur {
-	int age = 12;
+import java.time.LocalDate;
 
+public class Formateur {
+	String nom;
+	String prenom;
+	LocalDate naiss;
+	
+	public Formateur(String nom, String prenom, LocalDate naiss){
+		this.nom = nom;
+		this.prenom = prenom;
+		this.naiss = naiss;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public LocalDate getNaiss() {
+		return naiss;
+	}
+
+	public void setNaiss(LocalDate naiss) {
+		this.naiss = naiss;
+	}
+	
 }
