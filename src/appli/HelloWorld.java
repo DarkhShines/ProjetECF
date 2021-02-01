216951fb7709259ba732d8932175150df6beba57
DarkhShines <dardotalain@gmail.com>
2021-02-01 14:42:40 +0100

    Commit 1.2 Telle est la voix

--- src/appli/HelloWorld.java
+++ src/appli/HelloWorld.java
@@ -3,7 +3,7 @@ package appli;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World ! DarkhShines");
-		System.out.println("Ceci est la version 1.1");
-		System.out.println("Ainsi ai-je parlé.");
+		System.out.println("Ceci est la version 1.2");
+		System.out.println("Tel est la voix");
 	}
 }
