7e3c01ba7ff0a003448192d2717f8df05a3c21c7
DarkhShines <dardotalain@gmail.com>
2021-02-01 14:47:52 +0100

    2.21 GIGOWATTS

--- src/appli/HelloWorld.java
+++ src/appli/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World ! DarkhShines");
 		System.out.println("Ceci est la version 1.1");
 		System.out.println("Ainsi ai-je parlé.");
+		System.out.println("2.21 GIGOWATTTTSSSS ????");
 	}
 }
