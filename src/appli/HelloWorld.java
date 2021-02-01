798abc4c61d1cd592c6a3c2e12af69ef37d91d81
DarkhShines <dardotalain@gmail.com>
2021-02-01 14:16:33 +0100

    Personne hello world

---
+++ src/appli/HelloWorld.java
@@ -0,0 +1,8 @@
+package appli;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World ! DarkhShines");
+		System.out.println("nope");
+	}
+}
---
+++ src/metier/Personne/Personne.java
@@ -0,0 +1,7 @@
+package metier.Personne;
+
+public class Personne {
+	public static void main(String[] args) {
+		System.out.println("salut");
+	}
+}
