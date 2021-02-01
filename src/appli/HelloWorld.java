19a84b002766fb04be0239b01cf0a8cfdd237304
DarkhShines <dardotalain@gmail.com>
2021-02-01 14:21:27 +0100

    Ceci est la version 1.1 Ainsi ai-je parlé.

--- src/appli/HelloWorld.java
+++ src/appli/HelloWorld.java
@@ -3,6 +3,7 @@ package appli;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World ! DarkhShines");
-		System.out.println("nope");
+		System.out.println("Ceci est la version 1.1");
+		System.out.println("Ainsi ai-je parlé.");
 	}
 }
